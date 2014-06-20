404a13016eb96e69874f3af015a6b08c5dd87e1e
shockingpants <Jon@eecs-601-044.mit.edu>
2014-06-20 14:47:03 -0400

    Routine_imac

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ import math
 import pygame as pg
 
 space=cs.cellspace()
-C=space.add_cell(cs.cellp,(300,300),radius=10)
+C=space.add_cell(cs.cellp,(300,300),radius=4)
 #space.add_cell((200,240),angle=math.pi/2)
 #pg.draw.polygon(space.screen, (255,0,0,0), C.box.get_vertices(), 0)
 #pg.display.flip()
