f88af15429e60eaf6432e0f2f6750f3cd0390051
shockingpants <Jon@eecs-601-061.mit.edu>
2014-06-25 22:20:28 -0400

    Routine Imac

--- test/steps/diffusion.py
+++ test/steps/diffusion.py
@@ -112,3 +112,68 @@ def gen_geom():
 		tetrads[i] = r*1.0e6
 
 	return mesh
+
+model  = gen_model()
+tmgeom = gen_geom()
+rng = srng.create('mt19937', 512)
+rng.initialize(2903)
+
+sim = solvmod.Tetexact(model, tmgeom, rng)
+
+tpnts = numpy.arange(0.0, INT, DT)
+# Find how many "time points" we have
+ntpnts = tpnts.shape[0]
+# Create the data structure: iterations x time points x tet samples
+res = numpy.zeros((NITER, ntpnts, SAMPLE))
+
+# Fetch the index of the tetrahedron at the centre of the mesh
+ctetidx = tmgeom.findTetByPoint([0.0, 0.0, 0.0])
+# Run NITER number of iterations:
+for i in range(NITER):
+	sim.reset()
+	# Inject all molecules into the central tet:
+	sim.setTetCount(ctetidx, 'A', NINJECT)
+	for j in range(ntpnts):
+		sim.run(tpnts[j])
+		# Loop over the tetrahedrons we are saving data for
+		for k in range(SAMPLE):
+			# Save the concentration in the tetrahedron, in uM
+			res[i,j,k] = sim.getTetConc(tetidxs[k], 'A')*1.0e6
+
+def plotres(tidx):
+	# Check the time index is in range
+	if (tidx >= INT/DT):
+		print 'Time index is out of range.'
+		return
+
+	pylab.scatter(tetrads, res_mean[tidx], s=2)
+	pylab.xlabel('Radial distance of tetrahedron ($\mu$m)')
+	pylab.ylabel('Concentration in tetrahedron ($\mu$M)')
+	t = tpnts[tidx]
+	pylab.title('Unbounded diffusion. Time: ' + str(t) + 's')
+	# Plot the analytical solution
+	plotanlyt(t)
+	pylab.xlim(0,0, 10.0)
+	pylab.ylim(0.0)
+	pylab.show()
+
+def plotanlyt(t):
+	segs = 100
+	anlytconc = numpy.zeros((segs))
+	radialds = numpy.zeros((segs))
+	maxrad = 0.0
+	for i in tetrads:
+		if (i > maxrad): maxrad = i
+	maxrad *= 1e-6
+	intervals = maxrad/segs
+	rad = 0.0
+	for i in range((segs)):
+		# Find the analytical concentration, and convert to mol/L
+		anlytconc[i]=1.0e3*(1/6.022e23)*\
+			((NINJECT/(math.pow((4*math.pi*DCST*t),1.5)))*\
+			(math.exp((-1.0*(rad*rad))/(4*DCST*t))))
+		radialds[i] = rad*1e6
+		rad += intervals
+	pylab.plot(radialds, anlytconc, color = 'red')
+
+res_mean = numpy.mean(res, axis = 0)
